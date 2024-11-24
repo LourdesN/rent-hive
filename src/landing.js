21ab89d3548cc9f9460b7c48b1e777babd058ea4
Lourdes <lourdeswairimu@gmail.com>
2024-11-24 12:55:18 +0300

    Landing page

---
+++ src/landing.js
@@ -0,0 +1,18 @@
+function landing(){
+    return(
+        // <!-- Navbar-->
+        <div className="navbar" id="mynav">
+           <ul> 
+            <a href="#home" className="logo">Rent Hive</a>
+            <a href="#">Home</a>
+            <a href="#Properties">Properties</a>
+            <a href="#footer">List Property</a>
+            <a href="#footer">Contact Us</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i className="fa fa-bars"></i>
+              </a>
+           </ul>
+          </div>
+    );
+}
+export default landing;
