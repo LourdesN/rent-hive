15e8034ae4b86cc6c2946e61fa5eca4aaba86332
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-11-24 16:24:10 +0300

    Styled the navbar and the properties section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-import Landing from "./landing"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-function App() {
+import './App.css';
+import "./Assets/CSS/Navbar.css"
+import "./Assets/CSS/Properties.css"
+
+import { Routes, Route } from 'react-router-dom';
+import Landing from "./Landing Page/Home"
+
+function App() 
+{
   return (
-   <Landing/>
+    <>
+      <Routes>
+        <Route exact path='/' element={<Landing/>}></Route>
+      </Routes>
+    </>
   );
 }
 
