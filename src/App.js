e08fff732b3dedd0dc40318dc5a4288e9ced393d
Samuel Muigai <ndungu.muigai01@gmail.com>
2024-12-13 12:54:50 +0300

    Created the dashboard, navbar and sidebar. Moving onto styling the dashboard content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import "./Assets/CSS/Login.css"
 import "./Assets/CSS/Navbar.css"
 import "./Assets/CSS/Properties.css"
+import "./Assets/CSS/Sidebar.css"
 import "./Assets/CSS/Signup.css"
 
 import { Routes, Route } from 'react-router-dom';
@@ -15,6 +16,7 @@ import { Slide, ToastContainer } from 'react-toastify'
 import Landing from "./Landing Page/Home"
 import Login from "./Login/Login";
 import SignUp from "./Sign Up/SignUp";
+import Dashboard from "./Dashboard/Dashboard";
 
 function App() 
 {
@@ -25,6 +27,7 @@ function App()
         <Route exact path='/' element={<Landing/>}></Route>
         <Route exact path='/login' element={<Login/>}></Route>
         <Route exact path="/sign-up" element={<SignUp/>}></Route>
+        <Route exact path="/dashboard/*" element={<Dashboard/>}></Route>
       </Routes>
     </>
   );
