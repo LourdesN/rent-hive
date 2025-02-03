0df7df89c8a60204ccd905f092af85f38e21d127
Samuel Muigai <ndungu.muigai01@gmail.com>
2025-02-03 15:43:15 +0300

    Working on the tab section of the invoices page'

--- src/Dashboard/Tenants/Invoices/Invoices.js
+++ src/Dashboard/Tenants/Invoices/Invoices.js
@@ -1,8 +1,24 @@
+import { NavLink } from "react-router-dom"
+
 const Invoices = () =>
 {
     return(
         <>
             <h2>Invoices page</h2>
+            <ul className="nav nav-tabs justify-content-center">
+                <li className="nav-item">
+                    <NavLink className="nav-link">All invoices</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link">Pending invoices</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link">Paid invoices</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link">Overdue invoices</NavLink>
+                </li>
+            </ul>
         </>
     )
 }
