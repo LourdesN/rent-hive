9e0906a92feabe970442a4c53b0a012b785c7f1c
Samuel Muigai <ndungu.muigai01@gmail.com>
2025-02-04 10:42:06 +0300

    Styling the invoices nav

--- src/Dashboard/Tenants/Invoices/Invoices.js
+++ src/Dashboard/Tenants/Invoices/Invoices.js
@@ -48,7 +48,7 @@ const Invoices = () =>
 
     return (
         <>
-            <nav className="d-flex flex-column flex-sm-row justify-content-center align-items-center gap-3 p-3 bg-light shadow-sm rounded">
+            <nav className="d-flex flex-column flex-sm-row justify-content-center align-items-center gap-3 p-3">
                 {
                     filters.map((filter) => (
                         <button key={filter} onClick={() => setFilter(filter)} className={`btn ${invoiceFilter === filter ? "btn-primary" : "btn-outline-primary"} px-4 py-2`}>
