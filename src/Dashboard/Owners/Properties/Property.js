d1d61ddfdd7c276f5c613c9164491201ebb21bf4
Ndungu-Muigai <ndungu.muigai01@gmail.com>
2025-03-27 15:36:10 +0300

    Updated the styling of the owner indivicual property page

--- src/Dashboard/Owners/Properties/Property.js
+++ src/Dashboard/Owners/Properties/Property.js
@@ -4,9 +4,8 @@ import { useParams, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
 
-import ImageSlider from "../../Components/ImageSlider"
-
 import EditProperty from "./Edit property"
+import Carousel from "../../Components/Carousel"
 
 const Property = () =>
 {
@@ -52,52 +51,47 @@ const Property = () =>
 
     return(
         <div className="container mt-1">
-            {property?.images && <ImageSlider images={property.images}/>}
-
             <div className="row">
-                {/* Property details */}
-                <div className="col-md-6">
-                    <div className="card p-3 shadow-sm">
+                <div className="col-12 col-md-6">
+                    <div className="card p-2 shadow-sm gap-1">
+                        <Carousel images={property?.images || []}/>
                         <h1 className="lead fw-bold text-uppercase fs-4">{property?.name}</h1>
                         <p className="lead">{property?.description}</p>
+                        <p className="text-secondary">
+                            <strong>Property type: </strong>{property?.property_type}
+                        </p>
                         <h5 className="text-secondary">
-                            <strong>Location:</strong> {property?.location}
+                            <strong>Location: </strong>{property?.location}
                         </h5>
                         <h4 className="text-primary">Rent per month: {formatCurrency(property?.rent)}</h4>
-                        <div className="mt-1 d-flex gap-2">
-                            <button className="btn btn-success" onClick={()=> setEditModalOpen(true)}>Edit Property</button>
-                            <button className="btn btn-secondary" onClick={() => navigate(-1)}>Back</button>
-                        </div>
                     </div>
                 </div>
 
-                {/* Tenant section */}
                 {
                     property?.property_status === "Leased" &&
-                
-                    <div className="col-md-6">
-                        <div className="d-flex flex-column flex-lg-row gap-3">
-                            {/* Tenant details */}
-                            <div className="card p-3 shadow-sm flex-fill">
-                                <h5 className="text-center">Tenant Details</h5>
-                                <hr />
-                                <p className="mb-3"><strong>Name:</strong> {tenantDetails?.first_name} {tenantDetails?.last_name}</p>
-                                <p className="mb-3"><strong>Email:</strong> {tenantDetails?.email}</p>
-                                <p className="mb-3"><strong>Phone number:</strong> {tenantDetails?.phone_number}</p>
-                            </div>
 
-                            {/* Lease details */}
-                            <div className="card p-3 shadow-sm flex-fill">
-                                <h5 className="text-center">Lease Details</h5>
-                                <hr />
-                                <p className="mb-3"><strong>Start date:</strong> {leaseDetails?.start_date}</p>
-                                <p className="mb-3"><strong>End date:</strong> {leaseDetails?.end_date}</p>
-                                <p className="mb-3"><strong>Lease period:</strong> {leaseDetails?.duration}</p>
-                            </div>
+                    <div className="col-12 col-md-6 d-flex flex-column gap-2 gap-md-5 mt-2 mt-md-0">
+                        <div className="card p-3 shadow-sm gap-3 col-md-12">
+                            <h2 className="text-uppercase text-decoration-underline fs-5 fw-bold">Tenant details</h2>
+                            <p ><strong>Name:</strong> {tenantDetails?.first_name} {tenantDetails?.last_name}</p>
+                            <p ><strong>Email:</strong> {tenantDetails?.email}</p>
+                            <p ><strong>Phone number:</strong> {tenantDetails?.phone_number}</p>
+                        </div>
+                        <div className="card p-3 shadow-sm gap-3 col-md-12">
+                            <h2 className="text-uppercase text-decoration-underline fs-5 fw-bold">Lease details</h2>
+                            <p ><strong>Start date:</strong> {leaseDetails?.start_date}</p>
+                            <p ><strong>End date:</strong> {leaseDetails?.end_date}</p>
+                            <p ><strong>Lease period:</strong> {leaseDetails?.duration}</p>
                         </div>
                     </div>
                 }
             </div>
+            
+            <div className="d-flex justify-content-center gap-4 my-3">
+                <button className="btn btn-success" onClick={()=> setEditModalOpen(true)}>Edit property details</button>
+                <button className="btn btn-secondary" onClick={()=> navigate(-1)}>Back</button>
+            </div>
+            
 
             {/* Modal for editing property details */}
             {
